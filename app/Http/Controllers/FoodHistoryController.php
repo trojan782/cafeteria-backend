0c07d315829c58e29d5115516b79582cc084c05c
Muritala David <davmuri1414@gmail.com>
2021-08-03 13:44:24 -0700

    food history

---
+++ app/Http/Controllers/FoodHistoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FoodHistory;
+use Illuminate\Http\Request;
+
+class FoodHistoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\FoodHistory  $foodHistory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FoodHistory $foodHistory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\FoodHistory  $foodHistory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FoodHistory $foodHistory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\FoodHistory  $foodHistory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FoodHistory $foodHistory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\FoodHistory  $foodHistory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FoodHistory $foodHistory)
+    {
+        //
+    }
+}
